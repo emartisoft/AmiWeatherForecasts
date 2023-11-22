84a4a419742c6947f3cd74a91bcd6266879d9b76
emarti <dtemarti@gmail.com>
2023-11-22 14:04:54 +0300

    Add files via upload
    Enconding problem is solved

--- sc/include/funcs.h
+++ sc/include/funcs.h
@@ -3,7 +3,7 @@
 
 extern char** getArray(char* txt, char* delimiter, int count);
 extern char* replaceWord(const char* s, const char* oldW, const char* newW);
-extern char* convertTRChar(char* str);
 extern BOOL fileExist(STRPTR filePath);
+extern char* convertToLatin(char *str);
 
 #endif
