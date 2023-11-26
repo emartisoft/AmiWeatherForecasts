d6e9b50c83355a6120e4d266e1f9fbd604f2fc90
emarti <dtemarti@gmail.com>
2023-11-26 21:11:30 +0300

    Add files via upload

--- sc/include/funcs.h
+++ sc/include/funcs.h
@@ -5,5 +5,6 @@ extern char** getArray(char* txt, char* delimiter, int count);
 extern char* replaceWord(const char* s, const char* oldW, const char* newW);
 extern BOOL fileExist(STRPTR filePath);
 extern char* convertToLatin(char *str);
+extern char* convertToUTF8(char *str);
 
 #endif
